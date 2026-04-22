e813b9d2d8f63d9d152a04b9c56bebfef2860e92
ErikAndreasson246 <14EA0529@amal.se>
2026-04-22 13:29:43 +0200

    Påbörjat RangedTorn och Enemy script

--- tower-defense/Scripts/EnemyScript.cs
+++ tower-defense/Scripts/EnemyScript.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class EnemyScript : StaticBody2D
 {
+	[Export]
+	private int enemyHP = 3;
+	
+	[Export]
+	private float enemyMoveSpeed = 5f;
+
+
+
+
 	public override void _Ready() {
 
 		
@@ -10,19 +19,14 @@ public partial class EnemyScript : StaticBody2D
 
 	public override void _Process(double delta) {
 
-		
-	}
-
-	public override void _Input(InputEvent @event) {
-		
 		if (Input.IsKeyPressed(Key.D)) {
 			
-			this.Position = new Vector2(1, 0);
+			this.Position += new Vector2(enemyMoveSpeed, 0);
 		}
 
 		if (Input.IsKeyPressed(Key.A)) {
 			
-			this.Position = new Vector2(-1, 0);
+			this.Position += new Vector2(-enemyMoveSpeed, 0);
 		}
 	}
 }
