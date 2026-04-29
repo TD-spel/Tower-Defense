9c3dbf9a415f86bfb0ba7b255432fc2bd5b6b764
Maxnykvist08 <14MN0702@amal.se>
2026-04-29 08:37:19 +0200

    banna

--- tower-defense/Scripts/RangedTornScript.cs
+++ tower-defense/Scripts/RangedTornScript.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public partial class RangedTorn : StaticBody2D
 {
 
-	[Export] private List<Node2D> enemies = new List<Node2D>();
+	//[Export] private List<Node2D> enemies = new List<Node2D>();
 	public override void _Ready() {
 		
 
@@ -19,11 +19,11 @@ public partial class RangedTorn : StaticBody2D
 
 	public void OnArea2dBodyEntered(Node2D body) {
 		
-		enemies.Add(body);
+		//enemies.Add(body);
 	}
 
 	public void OnArea2dBodyExited(Node2D body) {
 		
-		enemies.Remove(body);
+		//enemies.Remove(body);
 	}
 }
