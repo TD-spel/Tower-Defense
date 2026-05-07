7c7ca4ab17c52537ced2056db3c314ed9bdacded
ErikAndreasson246 <14EA0529@amal.se>
2026-05-07 09:16:04 +0200

    Valkyrie

--- tower-defense/Scripts/ValkyrieTornScript.cs
+++ tower-defense/Scripts/ValkyrieTornScript.cs
@@ -5,12 +5,13 @@ public partial class ValkyrieTornScript : StaticBody2D
 {
 
 	private float hitSpeed = 1f;
+	private float hitSpeedModifier = -0.05f;
 
 	public override void _Ready() {
 		base._Ready();
 	}
 	public override void _Process(double delta) {
 		
-		Rotate(hitSpeed * .05f);
+		Rotate(hitSpeed * hitSpeedModifier);
 	}
 }
